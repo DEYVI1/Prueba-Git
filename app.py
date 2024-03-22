d0c410dc80aad153876f962b0786c82aaff93a90
DEYVI1 <deyvi-cabrera@live.com.mx>
2024-03-22 00:27:34 -0700

    agregamos la primary key en la base de datos

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contactos.db'
 db = SQLAlchemy(app)
 
-class Contacto(db.Model):
- #   id = db.Column(db.Integer, primary_key=True)
+class contacto(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     mensaje = db.Column(db.Text, nullable=False)
