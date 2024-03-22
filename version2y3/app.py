632db30d0f3e5911bd2e74714ef5858898d4ad70
DEYVI1 <deyvi-cabrera@live.com.mx>
2024-03-22 00:49:18 -0700

    Se agrega el direccionamiento del index en el .py pero persiste la falla

--- version2y3/app.py
+++ version2y3/app.py
@@ -5,21 +5,21 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contactos.db'
 db = SQLAlchemy(app)
 
-class contacto(db.Model):
+class Contacto(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nombre = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), nullable=False)
     mensaje = db.Column(db.Text, nullable=False)
 
-@app.route('/')
+@app.route('/version1')
 def index():
-    return render_template('index.html')
+    return render_template('/version1/index.html')
 
-@app.route('/productos')
+@app.route('/version1/productos')
 def productos():
-    return render_template('productos.html')
+    return render_template('/version1/productos.html')
 
-@app.route('/contacto', methods=['GET', 'POST'])
+@app.route('/version1/contacto.html', methods=['GET', 'POST'])
 def contacto():
     if request.method == 'POST':
         nombre = request.form['nombre']
@@ -30,8 +30,7 @@ def contacto():
         db.session.add(nuevo_contacto)
         db.session.commit()
         return '¡Gracias! Nos pondremos en contacto contigo pronto.'
-    return render_template('contacto.html')
+    return render_template('version 1/contacto.html')
 
 if __name__ == '__main__':
-    db.create_all()
     app.run(debug=True)
