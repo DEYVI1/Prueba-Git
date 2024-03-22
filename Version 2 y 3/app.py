2cee6e67323ec328069bd5344deece0c89e2acd2
DEYVI1 <deyvi-cabrera@live.com.mx>
2024-03-22 00:17:59 -0700

    modificado

--- Version 2 y 3/app.py
+++ Version 2 y 3/app.py
@@ -1,8 +1,8 @@
-#from flask import Flask, render_template, request
+from flask import Flask, render_template, request
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contactos.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contactos.db'
 db = SQLAlchemy(app)
 
 class Contacto(db.Model):
